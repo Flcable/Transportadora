0cbb3a0249c6a508eeea78b4145725bfab7c58a4
Flavio <flcable@gmail.com>
2016-03-28 20:08:02 -0300

    First Commit

---
+++ src/transportadora/Funcionario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transportadora;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Flavio
+ */
+public class Funcionario {
+    public int ID;
+    public String Nome;
+    public String CPF;
+    public String Cargo;
+    public Date DtNasc;
+    
+}
