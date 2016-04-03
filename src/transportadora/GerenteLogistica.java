a11ff95f392e1a3abb13442e27a656119c0cc5cb
Flavio <flcable@gmail.com>
2016-04-03 17:51:14 -0300

    #

--- src/transportadora/GerenteLogistica.java
+++ src/transportadora/GerenteLogistica.java
@@ -15,7 +15,18 @@ public class GerenteLogistica extends Funcionario {
     
     
     public double calcularSalario(){
-        double salarioFinal = 0; //Implementar regra de negocio para Gerente de logistica
+        double salarioFinal = 0; //Implementar regra de negocio para gerentes de vendas
+        double qtdCargaTransportada = 0;
+        double salarioMensal = 3000.00;
+        double cargaTransportada = salarioMensal*0.10;
+        double bonus = qtdCargaTransportada * cargaTransportada;
+        
+        if (qtdCargaTransportada > 0) {
+            salarioFinal = salarioMensal + bonus;
+        }else {
+            salarioFinal = salarioMensal;
+        }
+        
         return salarioFinal;
     }
     
