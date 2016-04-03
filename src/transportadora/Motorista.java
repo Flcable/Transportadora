a2aae7fd826858602468b89b5af3216e3c5cb6d9
Flavio <flcable@gmail.com>
2016-04-03 17:38:17 -0300

    Implementações do metodo calcularSalario nas demais classes

--- src/transportadora/Motorista.java
+++ src/transportadora/Motorista.java
@@ -13,8 +13,34 @@ public class Motorista extends Funcionario{
     public double SalarioHora;
     public double HoraAdicionais;
     
-    public double calcularSalario(){
-        double salarioFinal = 0; //Implementar regra de negocio para Motorista
+    
+    public double calcularSalario(double salarioHora, double horaTrabalhadas, double salarioFinal){
+        salarioHora = 15;
+        salarioFinal = 0;
+        double ht = 0;
+        boolean caminhao = true;
+        
+        
+        if (ht > 100 && caminhao == true){
+           
+            double bonusHora = (salarioHora*0.25) * 30;
+            double bonusCarga = (salarioHora*0.10) * 30;
+            double hm =  salarioHora*30;
+            salarioFinal =( hm +  bonusHora + bonusCarga)* ht;
+            
+        } else if (ht <= 100 || caminhao == true){
+            double bonusCarga = (salarioHora*0.10) * 30;
+            double hm =  salarioHora*30;
+            salarioFinal =( hm +  bonusCarga)* ht;
+            
+        } else if (ht > 100 || caminhao == false) {
+            double bonusHora = (salarioHora*0.25) * 30;
+            double hm =  salarioHora*30;
+            salarioFinal =( hm +  bonusHora )* ht;
+        }else {
+            double hm =  salarioHora*30;
+            salarioFinal = hm * ht;
+        }       
         return salarioFinal;
     }
 }
